bada5753bd613891b0b5c96c6c4dea71ea81b65f
Stephen M. Coakley <me@stephencoakley.com>
2017-04-17 11:51:29 -0500

    v0.2: Add all encoding validation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+extern crate encoding;
 extern crate getopts;
 
+use encoding::types::*;
 use getopts::*;
 use std::env;
 use std::fs::File;
@@ -10,10 +12,11 @@ use std::str;
 fn main() {
     let mut options = Options::new();
 
-    options.optflag("", "check-utf8", "Validate an UTF-8 file (default)");
-    options.optflag("", "check-utf8mb4", "Check for 4-byte characters in a UTF-8 file");
-    options.optopt("f", "file", "A file to parse, otherwise stdin is used", "FILE");
-    options.optflag("h", "help", "Show this help message");
+    options.optflag("v", "validate", "Verify the file is valid in the given encoding.");
+    options.optflag("", "check-utf8mb4", "Check for 4-byte characters in a UTF-8 file.");
+    options.optopt("e", "encoding", "Specify the file encoding. Defaults to UTF-8.", "ENCODING");
+    options.optopt("f", "file", "A file to parse, otherwise stdin is used.", "FILE");
+    options.optflag("h", "help", "Show this help message.");
 
     let matches = match options.parse(env::args()) {
         Ok(m) => m,
@@ -26,20 +29,42 @@ fn main() {
         return;
     }
 
+    // Get the input stream.
     let mut input: Box<Read> = match matches.opt_str("f") {
         Some(filename) => Box::new(File::open(filename).expect("given file is not readable")),
         None => Box::new(stdin()),
     };
+
+    // Get the encoding to use.
+    let encoding = match matches.opt_str("e").map(|s| s.to_lowercase()).as_ref().map(|s| s as &str) {
+        Some("utf16") => encoding::all::UTF_16LE,
+        Some(name) => {
+            match encoding::label::encoding_from_whatwg_label(name) {
+                Some(encoding) => encoding,
+                None => {
+                    println!("Unknown encoding: {}", name);
+                    return;
+                },
+            }
+        },
+        None => encoding::all::UTF_8,
+    };
+
+    if matches.opt_present("v") {
+        validate(&mut input, encoding);
+        return;
+    }
 
     if matches.opt_present("check-utf8mb4") {
         check_utf8mb4(&mut input);
         return;
     }
 
-    check_utf8(&mut input);
+    println!("No command given.");
 }
 
-fn check_utf8(reader: &mut Read) {
+fn validate(reader: &mut Read, encoding: &Encoding) {
+    let encoding_name = encoding.whatwg_name().unwrap_or("");
     let mut reader = BufReader::new(reader);
     let mut line = 1;
     let mut found = 0;
@@ -53,19 +78,21 @@ fn check_utf8(reader: &mut Read) {
 
         line += 1;
 
-        if str::from_utf8(&buf).is_err() {
+        // Validate the line using strict decoding.
+        if encoding.decode(&buf, DecoderTrap::Strict).is_err() {
             found += 1;
 
-            let mut lossy = String::from_utf8_lossy(&buf).into_owned();
+            // Lossily convert for display purposes.
+            let mut lossy = encoding.decode(&buf, DecoderTrap::Replace).unwrap();
             if !lossy.ends_with("\n") {
                 lossy.push('\n');
             }
 
-            print!("Invalid UTF-8 ({}): {}", line, lossy);
+            print!("line {}: invalid text: {}", line, lossy);
         }
     }
 
-    println!("Found {} lines containing invalid UTF-8.", found);
+    println!("Found {} lines containing invalid text for encoding {}.", found, encoding_name);
 }
 
 fn check_utf8mb4(reader: &mut Read) {
